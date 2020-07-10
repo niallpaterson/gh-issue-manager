e702bbdc2f3189540a5ca56c6d143a54c1d1cf2e
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-10 09:41:41 +0100

    chore: add routes

---
+++ src/container/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Routes.module.scss";
+import IssuePanel from "../IssuePanel"
+
+const Routes = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Routes;
