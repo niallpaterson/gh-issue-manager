c310e46497fc1626a65c716539f78a705813deb0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-25 16:53:55 +0100

    feat: add app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import styles from './App.module.scss';
+const App = () => {
+
+  return (
+   <div className={styles.pageWrapper}>
+   </div>
+  );
+  }
+
+export default App;
