6af603aaefe90c055988fa55aaca4437943e026e
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-25 16:55:53 +0100

    feat: add issue data cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './App.module.scss';
 const App = () => {
 
   const cleanLabels = (labels) => labels.map((label) => ({ name: label.name, color: label.color }));
+  const cleanIssues = (issues) => issues.map((issue) => ({ title: issue.title, number: issue.number, labels: cleanLabels(issue.labels), body: issue.body }));
   return (
    <div className={styles.pageWrapper}>
    </div>
