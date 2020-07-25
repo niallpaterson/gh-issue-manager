6904928518a02f25ad5dab881c2bb0c5d378c693
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-25 16:57:48 +0100

    style: add whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ const App = () => {
 
   const [repoLabels, setRepoLabels] = useState([]);
   const [issues, setIssues] = useState([]);
+
   const cleanLabels = (labels) => labels.map((label) => ({ name: label.name, color: label.color }));
   const cleanIssues = (issues) => issues.map((issue) => ({ title: issue.title, number: issue.number, labels: cleanLabels(issue.labels), body: issue.body }));
+
   const fetchIssues = (searchTerm) => {
     fetch(`https://api.github.com/repos/${repo}/issues`)
     .then((response) => response.json())
