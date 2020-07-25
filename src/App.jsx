dfa3ad41def688ce6d8542ddcb4f42f30e7389a0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-25 16:55:37 +0100

    feat: add label data cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styles from './App.module.scss';
 const App = () => {
 
+  const cleanLabels = (labels) => labels.map((label) => ({ name: label.name, color: label.color }));
   return (
    <div className={styles.pageWrapper}>
    </div>
