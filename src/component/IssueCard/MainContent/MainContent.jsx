8ac87682029a955f09eb2f0fb281485cc2b700db
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 12:03:18 +0100

    chore(main-content): prop types checks

--- src/component/IssueCard/MainContent/MainContent.jsx
+++ src/component/IssueCard/MainContent/MainContent.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styles from "./MainContent.module.scss";
 
 const MainContent = ({ title, body }) => <div className={styles.MainContent}><h3>{title}</h3><p>{body}</p></div>
 
+MainContent.PropTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string,
+},
 
 export default MainContent;
