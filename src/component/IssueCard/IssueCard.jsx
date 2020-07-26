285588faacd5ea06988485f3822b0b902a17a0c2
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 11:38:40 +0100

    style: move IssueCard  props to line list
    also, remove unnecessary curly braces around return

--- src/component/IssueCard/IssueCard.jsx
+++ src/component/IssueCard/IssueCard.jsx
@@ -3,13 +3,17 @@ import styles from "./IssueCard.module.scss";
 import MainContent from './MainContent';
 import SideBar from './SideBar';
 
-const IssueCard = ({ title, description, number, labels }) => {
-  return (
-      <article className={styles.IssueCard}>
-        <SideBar labels={labels} number={number} />
-        <MainContent title={title} description={description} />
-      </article>
-  );
+const IssueCard = ({
+  title,
+  body,
+  number,
+  labels,
+}) => (
+    <article className={styles.IssueCard}>
+      <SideBar labels={labels} number={number} />
+      <MainContent title={title} body={body} />
+    </article>
+);
 };
 
 export default IssueCard;
