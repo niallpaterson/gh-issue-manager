548f78676491c1f59261b9cd21899fc7fe4af62d
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 11:28:38 +0100

    chore: add prop type checks for label component

--- src/utilities/Label/Label.jsx
+++ src/utilities/Label/Label.jsx
@@ -1,7 +1,11 @@
+import PropTypes from 'prop-types';
 const Label = ({ labelName, color }) => (
   <div className={styles.label} style={{ background: color }}>{labelName}</div>
 );
 
-const Label = (props) => <div className={styles.label} style={{background: props.color}}>{props.labelName}</div>
+Label.propTypes = {
+  labelName: PropTypes.string.isRequired,
+  labelColo: PropTypes.string.isRequired,
+};
 
 export default Label;
