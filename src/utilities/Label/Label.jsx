cb734735f16b7bbf011d5f3eb6542072dfd6a5f2
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 11:28:09 +0100

    style: destructure label parameter
    use object destructuring to avoid 'this.props...'
    
    also moved return to own line because now over max line length

--- src/utilities/Label/Label.jsx
+++ src/utilities/Label/Label.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import styles from "./Label.module.scss";
+const Label = ({ labelName, color }) => (
+  <div className={styles.label} style={{ background: color }}>{labelName}</div>
+);
 
 const Label = (props) => <div className={styles.label} style={{background: props.color}}>{props.labelName}</div>
 
