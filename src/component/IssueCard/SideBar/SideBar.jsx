1a5af580b9f38215ba581f82d552d3be33fe67db
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 11:41:40 +0100

    style(sidebar): remove unnecessary return braces

--- src/component/IssueCard/SideBar/SideBar.jsx
+++ src/component/IssueCard/SideBar/SideBar.jsx
@@ -3,15 +3,13 @@ import styles from "./SideBar.module.scss";
 import IssueNumber from "../../../utilities/IssueNumber";
 import Label from "../../../utilities/Label";
 
-const SideBar = ({number, labels}) => {
-  return (
+const SideBar = ({ number, labels }) => (
 <aside className={styles.SideBar}>
   <IssueNumber issueNumber={number} />
   <div className={styles.LabelContainer}>
-    {labels.map((label, index) => <Label labelName={labels[index]}/>)}
+    {labels.map((label, index) => <Label key={index} labelName={label.name} color={label.color}/>)}
   </div>
 </aside>
-  )
-}
+);
 
 export default SideBar;
