557f37e7d4e14c3417edab9cfccab347a3d9dcbe
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 11:29:14 +0100

    style: replace double with single quotes

--- src/utilities/Label/Label.jsx
+++ src/utilities/Label/Label.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Label.module.scss';
+
 const Label = ({ labelName, color }) => (
   <div className={styles.label} style={{ background: color }}>{labelName}</div>
 );
