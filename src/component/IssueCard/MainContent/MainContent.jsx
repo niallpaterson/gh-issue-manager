6296a21b2c912aafb015ea60d2bc6377c8cdb236
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-26 12:02:30 +0100

    refactor(issue-card): change 'descrition' prop
    new name is 'body'; change made for consistency with data

--- src/component/IssueCard/MainContent/MainContent.jsx
+++ src/component/IssueCard/MainContent/MainContent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./MainContent.module.scss";
 
-const MainContent = ({ title, description }) => <div className={styles.MainContent}><h3>{title}</h3><p>{description}</p></div>
+const MainContent = ({ title, body }) => <div className={styles.MainContent}><h3>{title}</h3><p>{body}</p></div>
+
 
 export default MainContent;
