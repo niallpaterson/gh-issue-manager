ede8688d0368ab902e1b3bb2cc248b2228890a16
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-28 17:44:01 +0100

    refactor: move render to two lines
    because violated linting max length

--- src/component/IssueCard/MainContent/MainContent.jsx
+++ src/component/IssueCard/MainContent/MainContent.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import PropTypes from 'prop-types';
 import styles from "./MainContent.module.scss";
 
-const MainContent = ({ title, body }) => <div className={styles.MainContent}><h3>{title}</h3><p>{body}</p></div>
+const MainContent = ({ title, body }) => (
+  <div className={styles.MainContent}><h3>{title}</h3><p>{body}</p></div>
+);
 
 MainContent.propTypes = {
   title: PropTypes.string.isRequired,
