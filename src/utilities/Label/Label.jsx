082b62fcb96b12dad4444964d2aca63e2a122b44
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-28 17:42:19 +0100

    fix: label colors do not render
    needed to use parameter expansion on line 6

--- src/utilities/Label/Label.jsx
+++ src/utilities/Label/Label.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import styles from './Label.module.scss';
 
 const Label = ({ labelName, color }) => (
-  <div className={styles.label} style={{ background: color }}>{labelName}</div>
+  <div className={styles.label} style={{ background: `#${color}` }}>{labelName}</div>
 );
 
 Label.propTypes = {
   labelName: PropTypes.string.isRequired,
-  labelColo: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 export default Label;
