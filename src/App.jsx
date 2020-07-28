3ed89fcfdaab365c1493487948d0be1f8da45d05
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-28 17:49:45 +0100

    style: tidy App.js methods for linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import styles from './App.module.scss';
-import IssuePanel from "./container/IssuePanel"
+import IssuePanel from './container/IssuePanel';
 import SidePanel from './component/SidePanel';
 
 const App = () => {
-
   const [repo, setRepo] = useState(['cli/cli']);
   const [repoLabels, setRepoLabels] = useState([]);
   const [issues, setIssues] = useState([]);
 
   const cleanLabels = (labels) => labels.map((label) => ({ name: label.name, color: label.color }));
-  const cleanIssues = (issues) => issues.map((issue) => ({ title: issue.title, number: issue.number, labels: cleanLabels(issue.labels), body: issue.body }));
+  const cleanIssues = (rawIssues) => rawIssues.map((issue) => (
+    {
+      title: issue.title,
+      number: issue.number,
+      labels: cleanLabels(issue.labels),
+      body: issue.body,
+    }
+  ));
 
   const fetchIssues = (searchTerm) => {
-    fetch(`https://api.github.com/repos/${repo}/issues`)
-    .then((response) => response.json())
-    .then((data) => setIssues(cleanIssues(data)))
-    .catch((err) =>  { if (err) throw err; })
-  }
+    fetch(`https://api.github.com/repos/${searchTerm}/issues`)
+      .then((response) => response.json())
+      .then((data) => setIssues(cleanIssues(data)))
+      .catch((err) => { if (err) throw err; });
+  };
 
   const fetchLabels = (repo) => {
     fetch(`https://api.github.com/repos/${repo}/labels`)
-    .then((response) => response.json())
-    .then((labels) => setRepoLabels(cleanLabels(labels)))
+      .then((response) => response.json())
+      .then((labels) => setRepoLabels(cleanLabels(labels)))
+      .catch((err) => { if (err) throw err; });
+  };
+
   const fetchRepo = (searchTerm) => {
     fetch(`https://api.github.com/repos/${searchTerm}/issues`)
       .then((response) => { if (response.status === 200) setRepo(searchTerm); })
       .catch((err) => { if (err) throw err; });
   };
 
-  useEffect(() => ( fetchIssues(repo), fetchLabels(repo) ), [repo]);
+  useEffect(() => (fetchIssues(repo)), [repo]);
+  useEffect(() => (fetchLabels(repo)), [repo]);
 
   return (
-   <div className={styles.pageWrapper}>
-    <SidePanel repo={repo} repoLabels={repoLabels}/>
-    <main>
-      <h2>{`github.com/${repo}`}</h2>
-      <IssuePanel issues={issues}/>
-    </main>
-   </div>
+    <div className={styles.pageWrapper}>
+      <SidePanel fetchRepo={fetchRepo} repo={repo} repoLabels={repoLabels} />
+      <main>
+        <h2>{`github.com/${repo}`}</h2>
+        <IssuePanel issues={issues} />
+      </main>
+    </div>
   );
-  }
+};
 
 export default App;
