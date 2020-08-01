ef97aa63f11b016b4a999aaeb52b7f47f4cb641f
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-01 12:20:40 +0100

    style(issuepanel): replace double w/ single quotes

--- src/container/IssuePanel/IssuePanel.jsx
+++ src/container/IssuePanel/IssuePanel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './IssuePanel.module.scss';
 import IssueCard from '../../component/IssueCard';
 
 const IssuePanel = ({ issues }) => (
