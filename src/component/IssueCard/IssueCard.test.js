b85c197a8cffd88bd2e518db168d25a177917411
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-02 17:46:08 +0100

    test(issuecards): fix tests

--- src/component/IssueCard/IssueCard.test.js
+++ src/component/IssueCard/IssueCard.test.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import IssueCard from "./IssueCard";
+import React from 'react';
+import { shallow } from 'enzyme';
+import IssueCard from './IssueCard';
 
-describe("IssueCard tests", () => {
-  it("should render", () => {
-    expect(render(<IssueCard />)).toBeTruthy();
+describe('IssueCard tests', () => {
+  const issueCard = shallow(<IssueCard title={'testIssueCard'} number={'1'} />);
+
+  it('should render', () => {
+    expect(issueCard).toBeTruthy();
   });
 });
