228a2f0ef53a3cfd1151427e5d3bd5a5106f12de
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-02 17:46:55 +0100

    Updates

--- precache-manifest.00c71a131f35a2b66f1971ca96a91b18.js
+++ precache-manifest.00c71a131f35a2b66f1971ca96a91b18.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2367bce7847c9f1242a5a16389a8180a",
+    "revision": "d489c6f50cccfb888101f65ace626c9b",
     "url": "/gh-issue-manager/index.html"
   },
   {
-    "revision": "ec0b085f7e36202229fe",
-    "url": "/gh-issue-manager/static/css/main.747c82cb.chunk.css"
+    "revision": "0235701b079cf496c243",
+    "url": "/gh-issue-manager/static/css/main.37cb33fb.chunk.css"
   },
   {
     "revision": "d30f7f0ed7b51c98ff39",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gh-issue-manager/static/js/2.08226858.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ec0b085f7e36202229fe",
-    "url": "/gh-issue-manager/static/js/main.51af1285.chunk.js"
+    "revision": "0235701b079cf496c243",
+    "url": "/gh-issue-manager/static/js/main.11b5e258.chunk.js"
   },
   {
     "revision": "eb6d54bb567f1c4a8d60",
