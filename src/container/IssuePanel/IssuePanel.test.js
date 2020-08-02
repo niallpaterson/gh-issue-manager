56f673fc00af3a02ace513fa2c8c55e8366e4163
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-02 17:43:53 +0100

    test(issuepanel): fix tests

--- src/container/IssuePanel/IssuePanel.test.js
+++ src/container/IssuePanel/IssuePanel.test.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import IssuePanel from "./IssuePanel";
+import React from 'react';
+import { shallow } from 'enzyme';
+import IssuePanel from './IssuePanel';
 
-describe("IssuePanel tests", () => {
-  it("should render", () => {
-    expect(render(<IssuePanel />)).toBeTruthy();
+describe("Label tests", () => {
+  const issuePanel = shallow(<IssuePanel issues={[]}/>);
+
+  it('should render', () => {
+    expect(issuePanel).toBeTruthy();
   });
 });
