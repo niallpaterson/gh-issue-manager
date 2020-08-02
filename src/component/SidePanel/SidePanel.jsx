48ecdc1677f7083ee5eeda793f254355e75e6d2e
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-02 17:45:00 +0100

    chore(sidepanel): add proptypes

--- src/component/SidePanel/SidePanel.jsx
+++ src/component/SidePanel/SidePanel.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './SidePanel.module.scss';
 import RepoLabels from './RepoLabels';
 import SearchBar from './Searchbar';
 
-const SidePanel = ({ repo, repoLabels, fetchRepo }) => (
+const SidePanel = ({ repoLabels, fetchRepo }) => (
     <section className={styles.SidePanel}>
       <h1>Github Issue Manager</h1>
       <SearchBar fetchRepo={fetchRepo}/>
       <RepoLabels repoLabels={repoLabels}/>
     </section>
 );
+
+SidePanel.propTypes = {
+  fetchRepo: PropTypes.func.isRequired,
+  repoLabels: PropTypes.array,
+};
 
 export default SidePanel;
